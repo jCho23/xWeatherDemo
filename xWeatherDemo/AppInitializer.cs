9c330076d793cbf9d58db2159acad70eb01e270d
June <junecho.ucb@gmail.com>
2018-03-06 18:18:49 -0800

    Update

--- xWeatherDemo/AppInitializer.cs
+++ xWeatherDemo/AppInitializer.cs
@@ -27,7 +27,7 @@ namespace xWeatherDemo
                     .Android
 					// TODO: Update this path to point to your Android app and uncomment the
 					// code if the app is not included in the solution.
-					.ApkFile ("/Users/junecho/Desktop/Demos/XWeatherDemo.apk")
+                    .ApkFile ("/Users/june/Desktop/APKs/XWeatherDemo.apk")
 					.StartApp();
             }
 
